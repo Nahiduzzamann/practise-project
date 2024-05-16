7c92e7244eabf270699871029eb9ddd8b3bdcf97
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-05-16 12:08:08 +0600

    ok

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 
 async function main() {
   try {
+    // await mongoose.connect(config.database_url as string);
     app.listen(config.port, () => {
       console.log(`Example app listening on port ${config.port}`);
     });
@@ -11,5 +12,6 @@ async function main() {
     console.log(error);
   }
 
-  await mongoose.connect(config.database_url as string);
+ 
 }
+main()
